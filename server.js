72e382bc26aa6bf45b240ebc022b48a2ae8ddb0b
Micheletesta <michele.testa.micheletesta@gmail.com>
2024-10-26 14:50:33 +0200

    ADD gestione zeroTier

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import fastify from './app.js'
 import ngrok from "@ngrok/ngrok"
+import ip from "ip"
 await fastify.ready()
+const addr = ip.address()
 const port = process.env.PORT ? process.env.PORT : fastify.config.server.port
-fastify.listen(port, fastify.config.server.listenAddr ? fastify.config.server.listenAddr : '0.0.0.0')
+fastify.listen(port, addr ? addr : '0.0.0.0')
+
+//fastify.listen(port, fastify.config.server.listenAddr ? fastify.config.server.listenAddr : '0.0.0.0')
 
 // Establish connectivity
 
